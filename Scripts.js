2103cef5f4bba9ea4642c6bfbfb89746e18e9a32
laponte16 <54826992+laponte16@users.noreply.github.com>
2022-07-06 14:08:00 -0400

    Sigh

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,22 @@
-function onButtonDalmatian(){
-    document.getElementById("textRace").textContent = "jiji";
+var data = $("#textRace")
+$("#dalmatian").on("click", clickDalmatian());
+
+/*var texto = $("#textRace");
+botonDalmata = $("#dalmatian");
+botonDalmata.click(clickDalmatian());*/
+
+
+function clickDalmatian(){
+    /*data.fadeOut(1000, function(){});*/
+    document.getElementById("textRace").textContent = "jiji nation";
+    /*data.fadeIn(500, function(){});*/
+
 }
+
+/* 
+function onButtonDalmatian(){
+    document.getElementById("textRace").fadeOut("slow", function(){});
+    document.getElementById("textRace").textContent = "jiji";
+    document.getElementById("textRace").fadeIn("slow", function(){});
+}
+*/
