7624e27755429ad4a5d73d828360ddb75b01e8de
laponte16 <54826992+laponte16@users.noreply.github.com>
2022-07-06 16:49:37 -0400

    Images en breed

--- Scripts.js
+++ Scripts.js
@@ -3,6 +3,7 @@ x = $(document);
 x.ready(start);
 
 var textRace = $("#textRace")
+var imagenRace = $("#imagenRace")
 var contentBody = $(".contentBodyInfo")
 
 function start(){
@@ -16,24 +17,32 @@ $("#poodle").on("click", clickPoodle);
 
 function clickDalmatian(){
     contentBody.fadeOut(500);
-    window.setTimeout(function(){textRace.text("Dalmatas")},700);
+    window.setTimeout(function(){textRace.text("Dalmatas");},700);
+    imgUrl = "Images/dalmata.jpg"
+    window.setTimeout(function(){imagenRace.css("background-image","url(" + imgUrl + ")");},700);
     contentBody.fadeIn(1500);
 }
 
 function clickGolden(){
     contentBody.fadeOut(500);
     window.setTimeout(function(){textRace.text("Golden")},700);
+    imgUrl = "Images/golden.jpg"
+    window.setTimeout(function(){imagenRace.css("background-image","url(" + imgUrl + ")");},700);
     contentBody.fadeIn(1500);
 }
 
 function clickSalchicha(){
     contentBody.fadeOut(500);
     window.setTimeout(function(){textRace.text("Salchicha")},700);
+    imgUrl = "Images/salchicha.jpg"
+    window.setTimeout(function(){imagenRace.css("background-image","url(" + imgUrl + ")");},700);
     contentBody.fadeIn(1500);
 }
 
 function clickPoodle(){
     contentBody.fadeOut(500);
     window.setTimeout(function(){textRace.text("Poodle")},700);
+    imgUrl = "Images/poodle.jpg"
+    window.setTimeout(function(){imagenRace.css("background-image","url(" + imgUrl + ")");},700);
     contentBody.fadeIn(1500);
 }
